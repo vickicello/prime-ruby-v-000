4cdacddbd7dc1ff3a79054f65ea95ba6b91a6e14
Vicki Hamm <vicki.cello@gmail.com>
2018-06-21 19:10:51 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,16 +1,8 @@
 def prime?(n)
-prime_array = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
-p = 2
-if n < 2
-    return p
+prime_array = []
+    if n == prime
+      return true
+    else
+      return false
 end
 
-while (n % p == 0) && (p < n)
-    prime_array.push(p)
-    p += 1
-end
-#puts prime_array.inspect
-return prime_array
-end
-#prime_factors(600851475143)
-puts prime_factors(30)
